9ecd82fcf2a430b8bd32061ca3e7bbef7efb2c47
kat coveney <katcoveney.author@gmail.com>
2024-05-21 11:34:54 -0500

    links kitchen complete

--- main.js
+++ main.js
@@ -1,18 +1,62 @@
-const materials = ['Hylian Rice', 'Big Hearty Truffle', 'Tabantha Wheat', 'Raw Prime Meat', 'Hateno Cheese', 'Bird Egg']
+const { getRandomInt } = require("./utils.js")
+
+const materials = ['Hylian Rice', 'Big Hearty Truffle', 'Tabantha Wheat', 'Raw Prime Meat', 'Hateno Cheese', 'Bird Egg','Goat Butter', 'Fresh Milk', 'Cane Sugar', 'Raw Bird Thigh']
 const meals = []
+const recipes =[
+    {
+        name:'Mushroom Rice Balls',
+        ingredients:['Big Hearty Truffle','Hylian Rice']
+    },
+    {
+        name:'Cheesy Omlette',
+        ingredients:['Hateno Cheese','Bird Egg']
+    },
+    {
+        name:'Cheesy Hylian Pizza',
+        ingredients:['Hateno Cheese','Tabantha Wheat']
+    },
+    {
+        name:'Prime Meat and Rice Bowl',
+        ingredients:['Raw Prime Meat','Hylian Rice']
+        
+    },
+    {
+        name:'Egg Pudding',
+        ingredients:['Bird Egg', 'Fresh Milk', 'Cane Sugar']
+        
+    },
+    {
+        name:'Egg Tart',
+        ingredients:['Bird Egg', 'Tabantha Wheat', 'Cane Sugar','Goat Butter']
+        
+    },
+    {
+        name:'Chicken Egg Fried Rice',
+        ingredients:['Bird Egg', 'Hylian Rice', 'Raw Prime Meat']
+        
+    },
+    {
+        name:'Poultry Pilaf',
+        ingredients:['Bird Egg', 'Raw Bird Thigh', 'Goat Butter','Hylian Rice']
+        
+    },
+]
 
-const cook = (firstIngredient, secondIngredient) => {
-    if (firstIngredient === 'Hylian Rice' && secondIngredient === 'Big Hearty Radish') {
-        meals.push('Mushroom Rice Balls')
-    }
+// Function to pick a random meal and cook it
+const cookRandomMeal = () => {
+    const randomRecipe = recipes[getRandomInt(recipes.length)];
+    meals.push(randomRecipe.name)
 }
+// Specify the number of meals to generate
+const numberOfMealsToGenerate = 1
 
-cook(materials[1], materials[0])
+// Generate random meals
+for (let food = 0; food < numberOfMealsToGenerate; food++) {
+    cookRandomMeal()
+}
 
 console.log('MEALS:')
 console.log('---------------')
 for (const meal of meals) {
-    console.log(meel)
+    console.log(meal)
 }
-
-
---
+++ utils.js
@@ -0,0 +1,4 @@
+// Function to generate a random integer within a range
+const getRandomInt = (max) => Math.floor(Math.random() * max);
+
+module.exports = { getRandomInt }
